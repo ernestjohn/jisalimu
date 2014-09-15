0d687aef1fe2911627b64a4cf2ca6688c124a2f2
Ernest John Ndungu <ernjohm@gmail.com>
2014-09-15 15:04:53 -0700

    first commit - Ernesto

---
+++ JisalimuLibrary/JisalimuClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JisalimuLibrary
+{
+    public class JisalimuClient
+    {
+        //This is the official Jisalimu API abstraction for the Jisalimu Mobile, Windows
+
+    }
+}
