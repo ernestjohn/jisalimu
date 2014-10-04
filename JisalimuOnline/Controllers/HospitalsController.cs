373ba017ac019b03564210151225b2a31fe50aa4
Ernest John Ndungu <ernjohm@gmail.com>
2014-10-03 17:18:53 -0700

    update

--- JisalimuOnline/Controllers/HospitalsController.cs
+++ JisalimuOnline/Controllers/HospitalsController.cs
@@ -30,13 +30,6 @@ namespace JisalimuOnline.Controllers
              //THis is a function to get hospitals and index it into the user tags
             double LATITUDE = latitude, LONGITUDE = longitude, DISTANCE = distance;
 
-            //Raw JSON query into elasticsearch
-            string JSON_QUERY = "{\"query\":{\"filtered\":{\"query\":{\"match_all\":{}},\"filter\":{\"geo_distance\":{\"distance\":\""+ DISTANCE +"km\",\"geolocation\":{\"lat\":"+ LATITUDE +",\"lon\":"+ LONGITUDE + "}}}}}}";
-            //Create a hospital request object and index it on ElasticSearch
-
-           // var result = client.Search<Hospital>(q => q.QueryRaw(JSON_QUERY));
-            //var result = client.Search<Hospital>(q => q.MatchAll());
-
             var result = client.Search<Hospital>(s => s
                 .Filter(f => f
                   .GeoDistance(
@@ -55,32 +48,26 @@ namespace JisalimuOnline.Controllers
         [HttpGet]
         public IHttpActionResult GetHospitalsUser(double latitude, double longitude, double distance, string userid)
         {
-          Uri node = new Uri("http://quintelelastic.cloudapp.net:9200/", UriKind.Absolute);
+            var node = new Uri("http://quintelelastic.cloudapp.net:9200/", UriKind.Absolute);
 
-          ConnectionSettings settings = new ConnectionSettings(
-            node,
-            defaultIndex: "jisalimu"
-        );
+            var settings = new ConnectionSettings(
+               node,
+               defaultIndex: "jisalimu"
+           );
 
-         ElasticClient client = new ElasticClient(settings);
-
-            //Remember what the user id is for
+            var client = new ElasticClient(settings);
 
             //THis is a function to get hospitals and index it into the user tags
-            double LATITUDE = -1, LONGITUDE = 36, DISTANCE = 2;
+            double LATITUDE = latitude, LONGITUDE = longitude, DISTANCE = distance;
 
-            //Raw JSON query into elasticsearch
-            string JSON_QUERY = "{\"query\":{\"filtered\":{\"query\":{\"match_all\":{}},\"filter\":{\"geo_distance\":{\"distance\":\""+ DISTANCE +"km\",\"geolocation\":{\"lat\":"+ LATITUDE +",\"lon\":"+ LONGITUDE + "}}}}}}";
-            //Create a hospital request object and index it on ElasticSearch
-
-            var result = client.Search<Hospital>(q => q.QueryRaw(JSON_QUERY));
-           
-
-            //client.Search<Hospital>(r => 
-            //    r.From(0)
-            //    .Size(20)
-            //    .Query( o => 
-            //    o.Term(t => t. )));
+            var result = client.Search<Hospital>(s => s
+                .Filter(f => f
+                  .GeoDistance(
+                    n => n.geolocation,
+                    d => d.Distance(DISTANCE, GeoUnit.Kilometers).Location(LATITUDE, LONGITUDE)
+                  )
+                )
+              );
 
             //Return the hospitals in a HTTP OK-200 object
             return Ok(result.Documents);
